6ee061304781bf4304de55839f8a0ee3f26b1754
Javier Segura <javier@miolabs.com>
2022-07-21 22:50:33 +0200

    adding delete pvf function

--- Sources/PDFLib-Swift/PDFLib.swift
+++ Sources/PDFLib-Swift/PDFLib.swift
@@ -179,12 +179,17 @@ open class PDF
     
     // Virtual FS
         
-    public func createPVF( filename:String, data:Data) {
+    public func createPVF( filename:String, data:Data ) {
         var bytes = [UInt8](repeating: 0, count: data.count)
         data.copyBytes(to: &bytes, count: data.count)
         
         PDF_create_pvf( pdf, filename.cString(using: .utf8), 0, &bytes, data.count, "")
     }
+    
+    public func deletePVF( filename:String ) {
+        PDF_delete_pvf( pdf, filename.cString(using: .utf8), 0 )
+    }
+
     
     // Tables
     
