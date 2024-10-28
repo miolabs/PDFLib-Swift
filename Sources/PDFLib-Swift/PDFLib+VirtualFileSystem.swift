d88288af400bc4d883c4120f0f5ddf34b75283cf
Javier Segura <javier@miolabs.com>
2024-10-28 19:47:57 +0100

    fixing vfs

--- Sources/PDFLib-Swift/PDFLib+VirtualFileSystem.swift
+++ Sources/PDFLib-Swift/PDFLib+VirtualFileSystem.swift
@@ -10,10 +10,9 @@ import CPDFLib
 extension PDF
 {
     public func createPVF( filename:String, data:Data ) {
-        var bytes = [UInt8](repeating: 0, count: data.count)
-        data.copyBytes(to: &bytes, count: data.count)
-        
-        PDF_create_pvf( pdf, filename.cString(using: .utf8), 0, &bytes, bytes.count, "")
+        data.withUnsafeBytes { buffer in
+            PDF_create_pvf( pdf, filename.cString(using: .utf8), 0, buffer, data.count, "")
+        }
     }
     
     public func deletePVF( filename:String ) {
